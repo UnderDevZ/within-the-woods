94425eb10fa6c82c0804286c1f81d8f7571b1f13
UnderDevZ <41596787+UnderDevZ@users.noreply.github.com>
2020-05-29 14:58:46 +0100

    Added Raycasting Script for Gun
    Added Raycasting Script for gun, target system to be added later

---
+++ Special Delivery/Assets/GunScript.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+
+    public float damage = 100f;
+    public float range = 100f;
+
+    public Camera playerCam;
+
+    // Update is called once per frame
+    void Update()
+
+
+    {
+
+
+        if (Input.GetButtonDown("Fire1")) 
+
+        {
+
+            Shoot();
+
+        }
+
+        void Shoot() 
+        {
+
+            RaycastHit hit;
+            if (
+
+            Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range)) 
+            {
+
+                Debug.Log(hit.transform.name);
+            }
+        
+        }
+    }
+}
