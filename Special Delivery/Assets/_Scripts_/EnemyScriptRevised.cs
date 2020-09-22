46179431193ee625d5db542ec2c1a0506dab5657
UnderDevZ <41596787+UnderDevZ@users.noreply.github.com>
2020-09-22 14:56:39 +0100

    GitLog Tester
    Just a test

---
+++ Special Delivery/Assets/_Scripts_/EnemyScriptRevised.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScriptRevised : MonoBehaviour
+            
+{
+    private NavMeshAgent nav;
+
+    public GameObject Player;
+
+
+   
+    // Start is called before the first frame update
+    void Start()
+
+    {
+
+        nav = GetComponent<NavMeshAgent>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        nav.SetDestination(Player.transform.position);
+        
+    }
+}
